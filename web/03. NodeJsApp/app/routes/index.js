3a52d27d3f2c6c1822762b42209a0fe64b845680
MarselSidikov <sidikov.marsel@gmail.com>
2019-03-09 04:53:15 +0300

    clear

--- web/03. NodeJsApp/app/routes/index.js
+++
@@ -1,7 +0,0 @@
-// подключаем функцию из users_routes
-const usersRoutes = require('./users_routes');
-// создаем функцию для export-а, именно ее вызываем в index.js
-module.exports = function(app) {
-  // вызываем функцию из users_routes для обработки запросов
-  usersRoutes(app);
-};
